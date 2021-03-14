2decee3b3ef7f8472979ee271fa6a8e40123bb25
Davi Lucas <davilucasdlcreator@gmail.com>
2021-03-13 21:43:09 -0300

    :sparkles: feat paste code of adsense

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -7,7 +7,11 @@ interface HeadProps {
 export default function Head({ children }:HeadProps){
     return (
         <NextHead>
+            {/* Image Of WebSite */}
             <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+            {/* Google AdSense */}
+            <script data-ad-client="ca-pub-4951079875930530" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            {/* Children */}
             {children}
         </NextHead>
     )
